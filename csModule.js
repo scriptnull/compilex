4f9b521f50b68766e7bbfe2bdd6e3b468b3d7905
Muhammed Ali <malisimsek17@gmail.com>
2023-04-17 14:28:55 +0300

    C# add support for Linux
    Must have: mono C# compiler

--- csModule.js
+++ csModule.js
@@ -75,6 +75,48 @@ exports.compileCS = function ( envData ,  code , fn ) {
 							});		
 			       		}  	
 			});
+		}
+		if (envData.OS === 'linux') {
+
+			//compile cs code 
+			command = 'cd temp && mcs ' + filename + '.cs';
+			exec(command, function (error, stdout, stderr) {
+				if (error) {
+					if (exports.stats) {
+						console.log('INFO: '.green + filename + '.cs contained an error while compiling');
+					}
+					var out = { error: stderr };
+					fn(out);
+				} else {
+					var tempcommand = "cd temp && mono " + filename + ".exe";
+					exec(tempcommand, function (error, stdout, stderr) {
+						if (error) {
+
+							if (error.toString().indexOf('Error: stdout maxBuffer exceeded.') != -1) {
+								var out = { error: 'Error: stdout maxBuffer exceeded. You might have initialized an infinite loop.' };
+								fn(out);
+							} else {
+								if (exports.stats) {
+									console.log('INFO: '.green + filename + '.cs contained an error while executing');
+								}
+
+								var out = { error: stderr };
+								fn(out);
+							}
+						} else {
+							if (exports.stats) {
+								console.log('INFO: '.green + filename + '.cs successfully compiled and executed!');
+							}
+							var out = { output: stdout };
+							fn(out);
+						}
+					});
+				}
+
+			});
+		}
+
+	});
 
  			//compiling and exrcuiting source code
 
@@ -97,13 +139,11 @@ exports.compileCSWithInput = function ( envData , code , input ,  fn ) {
 	if(envData.OS === 'windows')
 	    {	    	    
 
-			//compile c code 
-			commmand = 'cd temp & csc ' + filename +'.cs';
-			exec(commmand , function ( error , stdout , stderr ){  
-				if(error)
-				{
-					if(exports.stats)
-					{
+			//compile cs code with input 
+			commmand = 'cd temp & csc ' + filename + '.cs';
+			exec(commmand, function (error, stdout, stderr) {
+				if (error) {
+					if (exports.stats) {
 						console.log('INFO: '.green + filename + '.cs contained an error while compiling');
 					}
 					var out = { error : stderr };
@@ -170,7 +210,67 @@ exports.compileCSWithInput = function ( envData , code , input ,  fn ) {
 			
 
 			});	 //end of csc exec    							
-	}
+		}
+		else if (envData.OS === 'linux') {
+
+			//compile cs code with input
+			command = 'cd temp && mcs ' + filename + '.cs';
+			exec(command, function (error, stdout, stderr) {
+				if (error) {
+					if (exports.stats) {
+						console.log('INFO: '.green + filename + '.cs contained an error while compiling');
+					}
+					var out = { error: stderr };
+					fn(out);
+				} else {
+					if (input) {
+						var inputfile = filename + 'input.txt';
+
+						fs.writeFile(path + inputfile, input, function (err) {
+							if (exports.stats) {
+								if (err)
+									console.log('ERROR: '.red + err);
+								else
+									console.log('INFO: '.green + inputfile + ' (inputfile) created');
+							}
+						});
+						var tempcommand = "cd temp && mono " + filename + ".exe";
+
+						exec(tempcommand + '<' + inputfile, function (error, stdout, stderr) {
+							if (error) {
+								if (error.toString().indexOf('Error: stdout maxBuffer exceeded.') != -1) {
+									var out = { error: 'Error: stdout maxBuffer exceeded. You might have initialized an infinite loop.' };
+									fn(out);
+								} else {
+									if (exports.stats) {
+										console.log('INFO: '.green + filename + '.cs contained an error while executing');
+									}
+									var out = { error: stderr };
+									fn(out);
+								}
+							} else {
+								if (exports.stats) {
+									console.log('INFO: '.green + filename + '.cs successfully compiled and executed !');
+								}
+								var out = { output: stdout };
+								fn(out);
+							}
+						});
+
+					} else //input not provided 
+					{
+						if (exports.stats) {
+							console.log('INFO: '.green + 'Input mission for ' + filename + '.cs');
+						}
+						var out = { error: 'Input Missing' };
+						fn(out);
+					}
+
+				}
+
+
+			});	 //end of mcs exec    							
+		}
 
 
 
